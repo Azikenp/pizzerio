7ff903911759bd9850a5fbaec9915d00f6308978
Azikenp <azikenp26@gmail.com>
2023-08-14 10:25:56 +0100

    adding menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { MenuList } from '../helper/MenuList'
+import MenuItem from '../components/MenuItem'
+import '../styles/Menu.css'
 
 function Menu() {
   return (
     <div className="menu">
         <h1 className='menuTitle'>Our Menu</h1>
+        <div className="menuList">
+            {MenuList.map((menus, key)=> {
+                return (
+                    <MenuItem    
+                        image={menus.image}
+                        price={menus.price}
+                        name={menus.name}
+                        key={key}
+                    />
+
+                )
+            })}
+        </div>
     </div>
   )
 }
